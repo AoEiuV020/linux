37f186ce13d441a5cfbe44e2a1de680f0b07750a
AoEiuV <490674483@qq.com>
2015-09-12 16:09:33 +0800

    AoEiuV020 commit...

--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 include header.mk
-n=truncate
+n=pipe
 $(n).$(TAREXT):$(n).c aoeiuv.h
 	gcc -Wall -Wpedantic --std=gnu11 --save-temps -o $(n).$(TAREXT) $(n).c
-	#.$(PS)$(n).$(TAREXT)
+	.$(PS)$(n).$(TAREXT)
 .PHONY:clean
 .IGNORE:clean
 clean:
--- pipe.c
+++ pipe.c
@@ -8,6 +8,28 @@
 #include "aoeiuv.h"
 int main(int argc, char **argv)
 {
-	
+	int pfd[2];
+	pid_t pid;
+	char string[20]="hello...";
+	char buf[80];
+	avsyscall(pipe,pfd);
+	avout("%d,%d;",pfd[0],pfd[1]);
+	avls("/proc/self/fd");
+	avsyscall(write,pfd[1],string,sizeof(string));
+	avsyscall(read,pfd[0],buf,sizeof(buf)-1);
+	avout("%s,",buf);
+	sprintf(string,"fork...");
+	pid=avsyscall(fork);
+	if(pid==0)
+	{
+		avout("child write:");
+		avsyscall(write,pfd[1],string,sizeof(string));
+	}
+	else
+	{
+		avout("farther read:");
+		avsyscall(read,pfd[0],buf,sizeof(buf)-1);
+		avout("%s,",buf);
+	}
 	return 0;
 }
