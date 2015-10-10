5cb8eebba7abf0d77b852cdf6b8d1b83b4c0f14b
AoEiuV020 <490674483@qq.com>
2015-10-11 04:25:54 +0800

    AoEiuV020 commit...

---
+++ fifo/read.c
@@ -0,0 +1,27 @@
+/*****************************************************
+	^> File Name: read.c
+	^> Author: AoEiuV020
+	^> Mail: 490674483@qq.com
+	^> Created Time: 2015/10/11 - 03:17:54
+****************************************************/
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	char buf[512];
+	int n;
+	memset(buf,0,sizeof(buf));
+	while(1)
+	{
+		if(n=read(STDIN_FILENO,buf,sizeof(buf)-1))
+		{
+			buf[n]=0;
+			write(STDOUT_FILENO,buf,strlen(buf));
+			fflush(stdout);
+		}
+		lseek(STDIN_FILENO,0L,SEEK_CUR);
+	}
+	return 0;
+}
+
